ff9324e85ab5faacb7dc34c5f243f526fba83685
Gilberto Díaz <diazp.gilberto@gmail.com>
2025-05-23 12:35:06 -0500

    working in submenu navbar

--- components/navbar/NavBar.jsx
+++ components/navbar/NavBar.jsx
@@ -1,97 +1,98 @@
 import { useState } from "react";
-
 import styles from "./NavBar.module.css";
 import { IoPeopleSharp } from "react-icons/io5";
 import { GrContactInfo } from "react-icons/gr";
 import { FaHistory, FaUniversity } from "react-icons/fa";
 import LogoMee from "../logoMee/LogoMee";
-import MenuItem from "../menuItem/MenuItem";
-import { Divider, Menu, Switch } from "antd";
 
 const NavBar = () => {
-  const [isOpenProductos, setIsOpenProductos] = useState(false);
-  const [isOpenRespaldo, setIsOpenRespaldo] = useState(false);
+  const [hoveredMenu, setHoveredMenu] = useState("");
 
-  const handleProductos = () => setIsOpenProductos(true);
-  const handleRespaldoEnergia = () => setIsOpenRespaldo(true);
-  
-  const handleMouseLeave = () => {
-    setIsOpenProductos(false);
-    setIsOpenSectores(false);
-  };
+  const handleMouseEnter = (menu) => () => setHoveredMenu(menu);
+  const handleMouseLeave = () => setHoveredMenu("");
 
   return (
-    <>
-      <div className={styles.navbar}>
-        <LogoMee></LogoMee>
-        <div className={styles.subnav}></div>
-        <div className={styles.optionsNav}>
-          <div className={styles.subnav}>
-            <a>
-              <button
-                className={styles.subnavbtn}
-                onMouseEnter={handleProductos}
-                onMouseLeave={handleMouseLeave}
+    <div className={styles.navbar}>
+      <LogoMee />
+
+      <div className={styles.optionsNav}>
+        <div className={styles.subnav}>
+          <button
+            className={styles.subnavbtn}
+            onMouseEnter={handleMouseEnter("productos")}
+            onMouseLeave={handleMouseLeave}
+          >
+            PRODUCTOS
+          </button>
+
+          <div
+            className={`${styles.subnavContent} ${
+              hoveredMenu === "productos" ? styles.slidebottom : ""
+            }`}
+            onMouseEnter={handleMouseEnter("productos")}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div>
+              <div
+                className={styles.category}
+                onMouseEnter={handleMouseEnter("respaldo")}
               >
-                PRODUCTOS
-              </button>
-            </a>
-            <div
-              onMouseEnter={handleProductos}
-              onMouseLeave={handleMouseLeave}
-              className={`${styles.subnavContent} ${
-                isOpenProductos ? styles.slidebottom : ""
-              }`}
-            >
-           
-              <div className={styles.slidebottom}>
-                <div className={styles.category}>
-                  <IoPeopleSharp className={styles.icon} />
-                  RESPALDO DE ENERGÍA
-                </div>
+                <IoPeopleSharp className={styles.icon} />
+                RESPALDO DE ENERGÍA
+                {hoveredMenu === "respaldo" ? (
+                  <ul className={styles.slideRight}>
+                    <li>UPS</li>
+                    <li>Baterias</li>
+                    <li>Inversores</li>
+                    <li>Rectificadores</li>
+                  </ul>
+                ) : (
+                  ""
+                )}
               </div>
+            </div>
 
-              <div className={styles.slidebottom}>
-                <div className={styles.category}>
-                  <GrContactInfo className={styles.icon} />
-                  CALIDAD DE ENERGÍA
-                </div>
+            <div className={styles.slidebottom}>
+              <div
+                className={styles.category}
+                onMouseEnter={handleMouseEnter("calidad")}
+              >
+                <GrContactInfo className={styles.icon} />
+                CALIDAD DE ENERGÍA
               </div>
+            </div>
 
-              <div className={styles.slidebottom}>
-                <div className={styles.category}>
-                  <FaHistory className={styles.icon} />
-                  CLIMA DE PRECISION
-                </div>
+            <div className={styles.slidebottom}>
+              <div
+                className={styles.category}
+                onMouseEnter={handleMouseEnter("clima")}
+              >
+                <FaHistory className={styles.icon} />
+                CLIMA DE PRECISIÓN
               </div>
+            </div>
 
-              <div className={styles.slidebottom}>
-                <div className={styles.category}>
-                  <FaUniversity className={styles.icon} />
-                  ACCIONAMIENTO ELÉCTRICO
-                </div>
+            <div className={styles.slidebottom}>
+              <div
+                className={styles.category}
+                onMouseEnter={handleMouseEnter("accionamiento")}
+              >
+                <FaUniversity className={styles.icon} />
+                ACCIONAMIENTO ELÉCTRICO
               </div>
             </div>
           </div>
-
-          <div className={styles.subnav}>
-            <button className={styles.subnavbtn}>SERVICIOS</button>
-          </div>
-
-          <div className={styles.subnav}>
-            <button className={styles.subnavbtn}>NOTICIAS</button>
-          </div>
-
-          <div className={styles.subnav}>
-            <button className={styles.subnavbtn}>CONTACTO</button>
-          </div>
-
-          <div className={styles.subnav}>
-            <button className={styles.subnavbtn}>ACERCA DE CENTIEL</button>
-          </div>
         </div>
+
+        {["SERVICIOS", "NOTICIAS", "CONTACTO", "ACERCA DE CENTIEL"].map(
+          (text) => (
+            <div key={text} className={styles.subnav}>
+              <button className={styles.subnavbtn}>{text}</button>
+            </div>
+          )
+        )}
       </div>
-    </>
+    </div>
   );
 };
 
